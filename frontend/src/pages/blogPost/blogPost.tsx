ba42c80d20cd4bec77995701279e56d1f1d02038
DhanushAdithiya <k.b.dhanush02@gmail.com>
2023-10-12 21:49:22 +0530

    Added Markdown Parsing + Small Changes to accessing Socials

--- frontend/src/pages/blogPost/blogPost.tsx
+++ frontend/src/pages/blogPost/blogPost.tsx
@@ -1,6 +1,10 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Navbar from "../../components/navbar/navbar";
 import { useParams } from "react-router-dom";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { LogoInstagram } from "react-ionicons";
+import { LogoLinkedin } from "react-ionicons";
 
 interface BlogData {
   title: string;
@@ -73,8 +77,18 @@ function App() {
   return (
     <>
       <h1 className="blog-title">{blogData?.title}</h1>
-      <p className="blog-content">{blogData?.content}</p>
+      <Markdown remarkPlugins={[remarkGfm]}>{blogData?.content}</Markdown>
       <h3 className="blog-author">Written by: {blogData?.author}</h3>
+      {userData?.instagram && (
+        <a href={userData.instagram}>
+          <LogoInstagram></LogoInstagram>
+        </a>
+      )}
+      {userData?.linkedin && (
+        <a href={userData.linkedin}>
+          <LogoLinkedin></LogoLinkedin>
+        </a>
+      )}
       <img src={userData?.profilePicture} alt="" />
     </>
   );
